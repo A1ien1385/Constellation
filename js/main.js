32da7c5ad698d615a7b75177fdd416370fb691f5
A1ien1385 <damian.zawadzki@tlen.pl>
2022-06-03 08:05:24 +0200

    Initialize class

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
+class Sky {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+  }
+}
 
-ctx.fillRect(0, 0, 100, 50);
+const sky = new Sky(document.querySelector("#canvas"));
